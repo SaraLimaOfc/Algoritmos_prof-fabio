669f35fe8eefc9180f0e00153e7ed74ed4e2e4d9
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-09-17 15:07:40 -0300

    Classe Pessoa

--- Classe.py
+++ Classe.py
@@ -6,44 +6,40 @@ class Pessoa:
         self.altura = altura
 
     def envelhecer(self, anos=1):
-        if anos < 0:
-            return "Anos deve ser um número positivo"
         self.idade += anos
         if self.idade < 21:
-            self.altura += 0.5 * anos
+            self.altura += anos * 0.5
 
-    def engordar(self, quilos):
-        if quilos < 0:
-            return "Quilos deve ser um número positivo"
-        self.peso += quilos
+    def engordar(self, peso):
+        self.peso += peso
 
-    def emagrecer(self, quilos):
-        if quilos < 0:
-            return "Quilos deve ser um número positivo"
-        if quilos > self.peso:
-            return "Não é possível emagrecer mais do que o peso atual"
-        self.peso -= quilos
+    def emagrecer(self, peso):
+        self.peso -= peso
+        if self.peso < 0:
+            self.peso = 0
 
-    def crescer(self, centimetros):
-        if centimetros < 0:
-            return "Centímetros deve ser um número positivo"
-        self.altura += centimetros
+    def crescer(self, altura):
+        self.altura += altura
 
     def __str__(self):
-        return f'Nome: {self.nome}, Idade: {self.idade}, Peso: {self.peso}kg, Altura: {self.altura}cm'
+        return (f'Nome: {self.nome}, Idade: {self.idade}, Peso: {self.peso}kg, Altura: {self.altura}cm')
 
 if __name__ == '__main__':
-    pessoa = Pessoa('Sara', 23, 100, 175)
+    pessoa = Pessoa(nome='Sara', idade=23, peso=100.0, altura=1.75)
     print(pessoa)
-
-    pessoa.envelhecer(3)
-    print("Após envelhecer 3 anos:", pessoa)
-
+    
+    pessoa.envelhecer()
+    print("Após envelhecer:")
+    print(pessoa)
+    
     pessoa.engordar(5)
-    print("Após engordar 5kg:", pessoa)
-
-    pessoa.emagrecer(2)
-    print("Após emagrecer 2kg:", pessoa)
-
+    print("Após engordar:")
+    print(pessoa)
+    
+    pessoa.emagrecer(3)
+    print("Após emagrecer:")
+    print(pessoa)
+    
     pessoa.crescer(2)
-    print("Após crescer 2cm:", pessoa)
+    print("Após crescer:")
+    print(pessoa)
